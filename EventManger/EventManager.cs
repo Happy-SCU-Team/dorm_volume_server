27bc792c1549ecc4c1ae3fe028eba5942bf36d64
QY W <uu57uu@yandex.com>
2024-04-03 14:22:13 +0800

    add event manger

---
+++ EventManger/EventManager.cs
@@ -0,0 +1,49 @@
+﻿namespace EventManger;
+
+public class EventManager
+{
+    public event EventHandler<EventEvent> onEventRecord;
+    public Level DisplayLevel { get; set; } = Level.Warn;
+    private List<EventMessage> events = new();
+    public EventManager Instance { get; private set; } = new();
+
+    private EventManager()
+    {
+        onEventRecord += EventManager_onEventRecord;
+    }
+
+    private void EventManager_onEventRecord(object? sender, EventEvent e)
+    {
+        if (e.EventMessage.level >= DisplayLevel)
+        {
+            Console.WriteLine($"[{e.EventMessage.level}]:{e.EventMessage.message}");
+        }
+    }
+
+    public void Add(string message)
+    {
+        Add(Level.Info,message);
+    }
+    public void Add(Level level,string message)
+    {
+        Add(new EventMessage { level=level,message=message});
+    }
+    public void Add(EventMessage eventMessage)
+    {
+        events.Add(eventMessage);
+        onEventRecord.Invoke(this,new EventEvent { EventMessage=eventMessage});
+    }
+}
+public enum Level
+{
+    Info,Warn,Error,
+}
+public struct EventMessage
+{
+    public Level level;
+    public string message;
+}
+public class EventEvent : EventArgs
+{
+    public EventMessage EventMessage;
+}
--- dorm_volume_server/QQ_Alert.cs
+++ dorm_volume_server/QQ_Alert.cs
@@ -9,13 +9,21 @@ namespace dorm_volume_server;
 
 internal static class QQ_Alert
 {
+    public static string URL = "http://localhost/alert"; 
     /// <summary>
     /// invoke QQ interface
     /// </summary>
     /// <param name="account"></param>
     /// <param name="volumeInfo"></param>
-    public static void alert(string account,VolumeInfo volumeInfo)
+    public static async void alert(string account,VolumeInfo volumeInfo)
     {
-
+        string jsonPayload = $"{{\"account\":{account},\"dorm_number\": {volumeInfo.dormId}}}";
+        using (HttpClient client = new HttpClient())
+        {
+            HttpResponseMessage response =
+                await client.PostAsync(
+                    URL,new StringContent(jsonPayload, Encoding.UTF8, "application/json")
+                );
+        }
     }
 }
